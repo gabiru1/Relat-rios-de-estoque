aafbaf2bbcbbf4cd0db64440ea9454e4c87eb113
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-06-23 10:00:46 -0300

    req 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# zfrom inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    expect = Product(
+        1,
+        "Cereal",
+        "Nescal",
+        "23/06/2022",
+        "23/12/2022",
+        "CE-9E45",
+        "local seco",
+    )
+
+    assert expect.__repr__() == (
+            f"O produto {expect.nome_do_produto}"
+            f" fabricado em {expect.data_de_fabricacao}"
+            f" por {expect.nome_da_empresa} com validade"
+            f" até {expect.data_de_validade}"
+            f" precisa ser armazenado {expect.instrucoes_de_armazenamento}."
+        )
