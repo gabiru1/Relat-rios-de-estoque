9902cd31da23790d3ff550c87d14385a4c6a4cd1
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-06-23 09:50:17 -0300

    fix

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -5,18 +5,18 @@ def test_cria_produto():
 
     product = Product(
         1,
-        "cereal",
-        "nescal",
+        "Cereal",
+        "Nescal",
         "23/06/2022",
         "23/12/2022",
-        "123456",
+        "CE-9E45",
         "local seco",
     )
 
     assert product.id == 1
-    assert product.nome_do_produto == 'cereal'
-    assert product.nome_da_empresa == 'nescal'
+    assert product.nome_do_produto == 'Cereal'
+    assert product.nome_da_empresa == 'Nescal'
     assert product.data_de_fabricacao == '23/06/2022'
     assert product.data_de_validade == '23/12/2022'
-    assert product.numero_de_serie == '123456'
+    assert product.numero_de_serie == 'CE-9E45'
     assert product.instrucoes_de_armazenamento == 'local seco'
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,6 +1,5 @@
-from inventory_report.inventory.product import Product
+# zfrom inventory_report.inventory.product import Product
 
-    
 
 def test_relatorio_produto():
     pass  # Seu teste deve ser escrito aqui
