393840103cd076e504d28ecd85ca251285aabb22
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-06-23 11:16:52 -0300

    finished

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,8 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import xml.etree.ElementTree as ET
+import json
 
 
 class Inventory():
@@ -8,9 +10,18 @@ class Inventory():
     def read_file(path):
 
         with open(path, encoding="utf-8") as file:
-            data = csv.DictReader(file, delimiter=",", quotechar='"')
 
-            return list(data)
+            if 'csv' in path:
+                data = csv.DictReader(file, delimiter=",", quotechar='"')
+
+                return list(data)
+            if 'json' in path:
+
+                return json.load(file)
+            if 'xml' in path:
+                xml = ET.parse(file.read())
+
+                return xml["dataset"]["record"]
 
     @classmethod
     def import_data(cls, path, type):
