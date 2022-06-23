3c9f7f78ff6281b57c9d7dab1a31b900f2798a8a
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-06-23 09:32:22 -0300

    req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,16 +1,21 @@
 from inventory_report.inventory.product import Product
 
-
-def test_cria_produto():
-
-    product = Product(
+product = Product(
         1,
         "cereal",
         "nescal",
         "23/06/2022",
         "23/06/2023",
         "123456",
-        "local seco",
+        "conservar em local seco",
     )
 
+
+def test_cria_produto():
     assert product.id == 1
+    assert product.nome_do_produto == 'cereal'
+    assert product.nome_da_empresa == 'nescal'
+    assert product.data_de_fabricacao == '23/06/2022'
+    assert product.data_de_validade == '23/06/2023'
+    assert product.numero_de_serie == '123456'
+    assert product.instrucoes_de_armazenamento == 'conservar em local seco'
